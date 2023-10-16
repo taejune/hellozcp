d4958b5b9344d0ef5fd76756c51ad33b31495b27
jigyoomin <genearth7@gmail.com>
2023-10-16 19:17:18 +0900

    hellozcp v2

--- src/main/java/com/sk/zcp/HelloZcpApplication.java
+++ src/main/java/com/sk/zcp/HelloZcpApplication.java
@@ -46,7 +46,7 @@ public class HelloZcpApplication {
 	@GetMapping("/home") 
 	public String home() {
 		logger.info("--------- home start ----------");
-		return "Hello ZCP";
+		return "Hello ZCP v2";
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
